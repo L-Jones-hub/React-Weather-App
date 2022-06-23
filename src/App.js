838116c65c01b8f2d399a64a12bc5441fef3aef2
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-06-23 10:14:50 -0500

    Added code from existing project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Lindsey Jones and is{" "}
-        <a
-          href="https://github.com/L-Jones-hub/React-Weather-App"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by Lindsey Jones and is{" "}
+          <a
+            href="https://github.com/L-Jones-hub/React-Weather-App"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
