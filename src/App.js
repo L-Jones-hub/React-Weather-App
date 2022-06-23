cbe1c28a259a24e37acbf6cca0a5bd6d34197007
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-06-23 09:35:37 -0500

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Lindsey Jones and is{" "}
+        <a
+          href="https://github.com/L-Jones-hub/React-Weather-App"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
