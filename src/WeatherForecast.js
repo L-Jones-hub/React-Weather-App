899c0fc8d17f03fdad48e6f391f6282e2c554cfb
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-06-23 11:10:03 -0500

    Added unit conversion

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "77bd80046e8a9087dc98db69d5679d68";
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
@@ -29,7 +29,7 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
+          {forecast.map(function(dailyForecast, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
